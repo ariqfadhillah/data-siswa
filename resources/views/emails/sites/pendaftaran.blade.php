d001dd35a7d8a70f2eca8d844eb88996f233044f
ariqfadhillah <ariqsaja99@gmail.com>
2020-02-25 14:08:06 +0700

    Added Mail google markdown mailable

--- resources/views/emails/sites/pendaftaran.blade.php
+++ resources/views/emails/sites/pendaftaran.blade.php
@@ -1,12 +1,20 @@
 @component('mail::message')
-# Pendaftaran siswa
+# Assalamualaikum, Afwan ya Maaf saya mengganggu nih. Semangat kerja lillah nya
 
-Selamat anda telah terdaftar memnjadi Relawan Hebat hari ini
+Testing website ini berhasil. . boleh kamu kirim screenshotnya ke ariq sekarang Jazakillah happy
 
-@component('mail::button', ['url' => 'http://ariqsss.com'])
+@component('mail::button', ['url' => 'https://api.whatsapp.com/send?phone=6285888836163&text=Saya%20telah%20mendapat%20pesan%20kamu%20mas%20ariq%20Allaahuma%20Baarik%20Fiihi.'])
 Klik disini
 @endcomponent
 
 Thanks,<br>
-{{ config('app.name') }}
+{{ config('app.Sekolah Relawan') }}
 @endcomponent
+
+<!-- MAIL_DRIVER=smtp
+MAIL_HOST=smtp.googlemail.com
+MAIL_PORT=465
+MAIL_USERNAME=ariqsaja99@gmail.com
+MAIL_PASSWORD=saitoasuka99
+MAIL_ENCRYPTION=ssl -->
+
